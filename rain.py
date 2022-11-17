275aa835717194776e9228bfb3cd17f64c177cb3
claudia-ribeiro <59340144+claudia-ribeiro@users.noreply.github.com>
2022-11-17 19:09:42 +0000

    relatórios

--- rain.py
+++ rain.py
@@ -4,8 +4,11 @@ from numpy.fft import fft, ifft
 from scipy.signal import find_peaks
 from datetime import datetime
 import pandas as pd
+from scipy.integrate import quad_vec
 import csv
 
 rain = pd.read_excel('MATLAB/rain.xlsx')
-
-
+x0 = 0
+x1 = 
+rain_integrated, err = quad_vec(rain, xo, x1)
+print(rain_integrated)
