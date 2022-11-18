3ac023b37a26621874deacdbb3fe14b4fa691e6f
Mafalda Ferreira <up201806395@g.uporto.pt>
2022-11-18 23:45:22 +0000

    Forecast + rain

--- rain.py
+++ rain.py
@@ -7,38 +7,47 @@ from scipy.signal import find_peaks
 from datetime import datetime
 import pandas as pd
 from scipy.integrate import quad_vec
+import matplotlib.dates as mdates
 import csv
 
 
 def s_to_datetime(date_string):
     from datetime import datetime
 
-    datetime_object = datetime.strptime(date_string, ' %d-%b-%Y %H:%M')
+    datetime_object = datetime.strptime(date_string, '%d-%b-%Y %H:%M')
     return datetime_object
 
 
 fhand = open("MATLAB/Forecast.csv", "r")
-forecast_dates = []
-#line = fhand.readline()
+
+forecast_dates = [] #u
+rain_amount = [] #r_a
+aux = [] 
 f_line = fhand.readlines()
-#while line != "":
- #   forecast_dates.append(line[:17])
-  #  line = fhand.readline()
+
 
 for line in f_line:
+    aux.append(line.strip().split(","))
     forecast_dates.append(line[:17])
-    #line = fhand.readline()
+    
+del forecast_dates[0]
+forecast_dates = list(map(lambda x: s_to_datetime(x), forecast_dates))
 
-#print(forecast_dates)
+del aux[0]
+for i in aux:
+    rain_amount.append(float(i[-1]))
+    
+print(len(forecast_dates))
+print(len(rain_amount))
+#print(rain_amount)
 
-for i in range(len(forecast_dates)-1):
-    forecast_dates[i] = s_to_datetime(forecast_dates[i+1])
-
-print(forecast_dates[1])
-
-t = np.linspace(0.0, 15, num=30)
-x0 = 0
-x1 = 15
-# rain_integrated, err = quad_vec(rain, x0, x1)
-# plt.plot(dates, rain)
-# plt.show()
+date_time = pd.to_datetime(forecast_dates)
+    
+DF = pd.DataFrame()
+DF['value'] = rain_amount
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=2))
+DF = DF.set_index(date_time)
+plt.plot(DF)
+plt.gcf().autofmt_xdate()
+plt.show()
