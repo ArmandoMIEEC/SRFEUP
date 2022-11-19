66376b9d17db9a240afdb2efa5eab45be613d164
claudia-ribeiro <59340144+claudia-ribeiro@users.noreply.github.com>
2022-11-19 18:11:06 +0000

    .

--- PYTHON/sr.py
+++ PYTHON/sr.py
@@ -3,6 +3,10 @@ import numpy as np
 from numpy.fft import fft, ifft
 from datetime import datetime
 import matplotlib.dates as mdates
+import pandas as pd
+import matplotlib.dates as mdates
+from scipy import integrate
+from scipy.integrate import quad
 
 # Signal
 with open('sat_data/sem12_sinal.txt') as f:
@@ -35,6 +39,8 @@ for line in signal_lines:
 
 for date in signal_dates:
     signal_datetime.append(datetime.strptime(date, '%d-%b-%Y %H:%M:%S'))
+
+for date in signal_dates23:
     signal_datetime23.append(datetime.strptime(date, '%d-%b-%Y %H:%M:%S'))
 
 X = fft(signal)
@@ -47,31 +53,31 @@ ts = 1.0 / sr
 t = np.arange(0, 1, ts)
 freq = n / T
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-#plt.stem(freq, np.abs(X), 'k', \
- #        markerfmt=" ", basefmt="-k")
-#ax.set_xlabel('xlabel', fontdict=dict(weight='bold'))
-#ax.set_ylabel('ylabel', fontdict=dict(weight='bold'))
-#plt.stem(freq, np.abs(X), 'b',
-  #       markerfmt=" ", basefmt="-b")
-#plt.xlabel('Frequência (Hz)')
-#plt.ylabel('Amplitude da FFT')
-#plt.xlim(-0.1, 10)
-#plt.savefig('fft_sinal.png', dpi=1000)
-#plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# plt.stem(freq, np.abs(X), 'k', \
+#        markerfmt=" ", basefmt="-k")
+# ax.set_xlabel('xlabel', fontdict=dict(weight='bold'))
+# ax.set_ylabel('ylabel', fontdict=dict(weight='bold'))
+# plt.stem(freq, np.abs(X), 'b',
+#       markerfmt=" ", basefmt="-b")
+# plt.xlabel('Frequência (Hz)')
+# plt.ylabel('Amplitude da FFT')
+# plt.xlim(-0.1, 10)
+# plt.savefig('fft_sinal.png', dpi=1000)
+# plt.show()
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-#plt.plot(signal_datetime, ifft(X), 'k')
-#ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
-#ax.set_xlabel('xlabel', fontdict=dict(weight='bold'))
-#ax.set_ylabel('ylabel', fontdict=dict(weight='bold'))
-#plt.xlabel('Data (dia/mês)')
-#plt.ylabel('Potência Recebida (dBm)')
-#fig.autofmt_xdate()
-#plt.savefig('sinal_comp_ant.png', dpi=1000)
-#plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# plt.plot(signal_datetime, ifft(X), 'k')
+# ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+# ax.set_xlabel('xlabel', fontdict=dict(weight='bold'))
+# ax.set_ylabel('ylabel', fontdict=dict(weight='bold'))
+# plt.xlabel('Data (dia/mês)')
+# plt.ylabel('Potência Recebida (dBm)')
+# fig.autofmt_xdate()
+# plt.savefig('sinal_comp_ant.png', dpi=1000)
+# plt.show()
 
 # Noise
 with open('sat_data/sem23_ruido.txt') as f:
@@ -86,26 +92,71 @@ for date in noise_dates:
     noise_datetime.append(datetime.strptime(date, '%d-%b-%Y %H:%M:%S'))
 
 Y = fft(noise)
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-#plt.plot(noise_datetime, ifft(Y), 'k', label='sinal')
-#plt.plot(noise_datetime, ifft(X23), color='red', label='ruído')
-#plt.plot()
-#ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
-#ax.set_xlabel('xlabel', fontdict=dict(weight='bold'))
-#ax.set_ylabel('ylabel', fontdict=dict(weight='bold'))
-#plt.xlabel('Data (dia/mês)')
-#plt.ylabel('Potência (dBm)')
-#plt.legend()
-#fig.autofmt_xdate()
-#plt.savefig('ruido_comp_ant.png', dpi=1000)
-#plt.show()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(noise_datetime, ifft(Y), 'k', label='sinal')
+plt.plot(signal_datetime23, ifft(X23), color='red', label='ruído')
+plt.plot()
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+ax.set_xlabel('xlabel', fontdict=dict(weight='bold'))
+ax.set_ylabel('ylabel', fontdict=dict(weight='bold'))
+plt.xlabel('Data (dia/mês)')
+plt.ylabel('Potência (dBm)')
+plt.legend()
+fig.autofmt_xdate()
+plt.savefig('ruido_comp_ant.png', dpi=1000)
+plt.show()
 
 signal23 = [float(x) for x in signal23]
 noise = [float(x) for x in noise]
-mean_signal = sum(signal23)/len(signal23)
-mean_noise = sum(noise)/len(noise)
+mean_signal = sum(signal23) / len(signal23)
+mean_noise = sum(noise) / len(noise)
 print(mean_signal)
 print(mean_noise)
 
 
+def s_to_datetime(date_string):
+    from datetime import datetime
+
+    datetime_object = datetime.strptime(date_string, '%d-%b-%Y %H:%M')
+    return datetime_object
+
+
+fhand = open("../MATLAB/Forecast.csv", "r")
+
+forecast_dates = []  # u
+rain_amount = []  # r_a
+aux = []
+f_line = fhand.readlines()
+
+for line in f_line:
+    aux.append(line.strip().split(","))
+    forecast_dates.append(line[:17])
+
+del forecast_dates[0]
+forecast_dates = list(map(lambda x: s_to_datetime(x), forecast_dates))
+
+del aux[0]
+for i in aux:
+    rain_amount.append(float(i[-1]))
+
+print(len(forecast_dates))
+print(len(rain_amount))
+# print(rain_amount)
+
+date_time = pd.to_datetime(forecast_dates)
+#rain_amount = integrate.cumulative_trapezoid(rain_amount)
+#rain_amount = np.append(rain_amount, rain_amount[-1])
+
+DF = pd.DataFrame()
+DF['value'] = rain_amount
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=2))
+DF = DF.set_index(date_time)
+plt.plot(DF)
+plt.plot(signal_datetime23, ifft(X23), color='red', label='ruído')
+plt.plot(noise_datetime, ifft(Y), 'k', label='sinal')
+plt.xlabel('Data (dia/mês)')
+plt.ylabel('Quantidade de chuva (mm)')
+plt.gcf().autofmt_xdate()
+plt.show()
--- rain.py
+++ rain.py
@@ -9,7 +9,7 @@ import pandas as pd
 from scipy.integrate import quad_vec
 import matplotlib.dates as mdates
 import csv
-
+import scipy.integrate as integrate
 
 def s_to_datetime(date_string):
     from datetime import datetime
@@ -42,7 +42,12 @@ print(len(rain_amount))
 #print(rain_amount)
 
 date_time = pd.to_datetime(forecast_dates)
-    
+rain_amount = integrate.cumulative_trapezoid(rain_amount)
+rain_amount = np.append(rain_amount, rain_amount[-1])
+#rain_amount = np.trapz(rain_amount)
+print(rain_amount)
+
+
 DF = pd.DataFrame()
 DF['value'] = rain_amount
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
