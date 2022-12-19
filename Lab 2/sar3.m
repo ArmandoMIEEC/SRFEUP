4d47994ea98010eb22d05f5ece00c71857a79ba2
claudia-ribeiro <59340144+claudia-ribeiro@users.noreply.github.com>
2022-12-19 19:02:53 +0000

    still not working...

--- Lab 2/sar3.m
+++ Lab 2/sar3.m
@@ -7,7 +7,7 @@ x = (-32000:32000-1)'*0.5;
 d = sqrt(x.*x*ones(1,1200)+ones(64000,1)*r.*r);
 A = (abs(atan2(x*ones(1,1200), ones(64000,1)*r))*180/pi <= 20) .* (abs(atan2(x*ones(1,1200), ones(64000,1)*r))*180/pi <= 20);
 s=A.*exp(-2j*pi*2*d/(300/fc));
-%res = zeros(64000-1,1200);
+res = zeros(64000-1,1200);
 
 
 
@@ -27,22 +27,20 @@ figure(4);
 ch_c = ifft(fftshift(ch_cf,2),[],2);
 figure(5);
 image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch_c'),1))/10000);
-%--------correct until here---------
 
 for k = 1:14001; ch_cf(:,k) = interp1((1:1400)'+100, ch_ef(:,k), ((1:1200)'+100)/sqrt(1-(300/2340/2*fx(k))^2));end
 ch_c = ifft(fftshift(ch_cf,2), [], 2);
 figure(6);
 image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch_cf'),1))/10000); 
 figure(7);
-image(abs(ch_c)'/100);
+image(abs(ch_c)/100);
 
 CH = [zeros((64000-14000)/2, 1200); ch_c(:,end:-1:1).'; zeros((64000-14000)/2-1, 1200)];
 
+%%
 res = circshift(ifft(fft(conj(CH)).*conj(fft(s))), -32000);
 figure(8);
 image(((1:1200)+100)*2.5, (-32000+1:32000-1)'*0.05, filter(fir1(128, 0.02),1,abs(res))/10000); axis equal
-figure(14)
-image(((1:1200)+100)*2.5, (-32000+1:32000-1)'*0.05, filter(fir1(128, 0.05),1,abs(res))/10000); axis equal
 
 ch_x = [zeros(100,14001); ch; zeros(200,14001)];
 ch_xf = fftshift(fft(ch_x,[],2),2);
@@ -50,7 +48,7 @@ ch_xf = fftshift(fft(ch_x,[],2),2);
 figure(10)
 image(abs(ch_xf')/10000);
 %%
-dn = round(1500*(1./sqrt(1-(300/fc/2*fx').^2)-1));
+dn = round(1500*(1./sqrt(1-(300/fc/2*fx).^2)-1));
 nn = 1500 - dn;
 ch_vf = zeros(size(ch_xf));
 for k=1:14001; dummy = fftshift(fft(ch_xf(:,k))); dummy = dummy(1:nn(k))+floor(dn(k)/2); ch_vf(1:nn(k),k)=ifft(fftshift(dummy)); end
@@ -62,4 +60,4 @@ CH = [zeros((64000-14000)/2, 1200); ch_c(:,end:-1:1).'; zeros((64000-14000)/2-1,
 res_c = res;
 res = circshift(ifft(fft(conj(CH)).*conj(fft(s))), -32000);
 figure(12)
-image(((1:1200)+100)*2.5, (-32000+1:32000-1)'*0.05, filter(fir1(128,0.05),1,abs(res))/10000); axis equal;
+image(((1:1200)+100)*2.5, (-32000+1:32000-1)'*0.05, filter(fir1(128,0.02),1,abs(res))/10000); axis equal;
