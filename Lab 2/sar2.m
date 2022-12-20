2eb61c8e1ecb0fd7ce136b6a5954e85e3e9d786e
claudia-ribeiro <59340144+claudia-ribeiro@users.noreply.github.com>
2022-12-20 22:37:03 +0000

    figure

--- Lab 2/sar2.m
+++ Lab 2/sar2.m
@@ -18,10 +18,10 @@ fx = 0:1/delta_x/size(ch, 2):1/delta_x*(1-1/size(ch,2));
 fx = fx - ceil(length(fx)/2)*1/delta_x/size(ch,2);
 for k = 1:14001; ch_cf(:,k) = interp1((1:1400)'+100, ch_ef(:,k), ((1:1200)'+100)/sqrt(1-(300/2340/2*fx(k))^2));end
 figure(2);
-%image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch'),1))/10000); 
-%figure(3);
-%image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch_cf'),1))/10000); 
-%figure(4);
+image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch'),1))/10000); 
+figure(3);
+image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch_cf'),1))/10000); 
+figure(4);
 ch_c = ifft(fftshift(ch_cf,2),[],2);
 figure(5);
 image(((1:1200)+100)*2.5, ((0:14000)'-7000)/14001/0.05, abs(fftshift(fft(ch_c'),1))/10000);
